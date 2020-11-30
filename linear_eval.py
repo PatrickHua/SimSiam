8185c75b26b509da76c878b2715d0c3560649854
Patrick <patrickhua.ty@gmail.com>
2020-12-01 01:42:46 +0800

    eval

--- linear_eval.py
+++ linear_eval.py
@@ -23,15 +23,6 @@ def main(args):
         train=True, 
         download=args.download # default is False
     )
-
-    train_loader = torch.utils.data.DataLoader(
-        dataset=train_set,
-        batch_size=args.batch_size,
-        shuffle=True,
-        num_workers=args.num_workers,
-        pin_memory=True,
-        drop_last=True
-    )
     test_set = get_dataset(
         args.dataset, 
         args.data_dir, 
@@ -40,6 +31,23 @@ def main(args):
         download=args.download # default is False
     )
 
+
+
+    if args.debug:
+        args.batch_size = 20
+        args.num_epochs = 2 
+        args.num_workers = 0
+        train_set = torch.utils.data.Subset(train_set, range(0, args.batch_size)) # take only one batch
+        test_set = torch.utils.data.Subset(test_set, range(0, args.batch_size))
+    
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_set,
+        batch_size=args.batch_size,
+        shuffle=True,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        drop_last=True
+    )
     test_loader = torch.utils.data.DataLoader(
         dataset=train_set,
         batch_size=args.batch_size,
@@ -53,7 +61,9 @@ def main(args):
 
     # define model
     # model = get_model(args.model, args.backbone)
-    backbone = get_backbone(args.backbone, castrate=True)
+    backbone = get_backbone(args.backbone, castrate=False)
+    in_features = backbone.fc.in_features
+    backbone.fc = nn.Identity()
     model = backbone
     assert args.eval_from is not None
     save_dict = torch.load(args.eval_from, map_location='cpu')
@@ -61,14 +71,17 @@ def main(args):
     print(msg)
     model = model.to(args.device)
     model = torch.nn.DataParallel(model)
-    if torch.cuda.device_count() > 1: model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
-    
-    breakpoint()
+    # if torch.cuda.device_count() > 1: model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    model.eval()
+
+    classifier = nn.Linear(in_features=in_features, out_features=10, bias=True).to(args.device)
+    classifier = torch.nn.DataParallel(classifier)
+    # breakpoint()
 
 
     # define optimizer
     optimizer = get_optimizer(
-        args.optimizer, model, 
+        args.optimizer, classifier, 
         lr=args.base_lr*args.batch_size/256, 
         momentum=args.momentum, 
         weight_decay=args.weight_decay)
@@ -81,16 +94,23 @@ def main(args):
         optimizer, args.num_epochs, eta_min=0)
 
     loss_meter = AverageMeter(name='Loss')
-
+    acc_meter = AverageMeter(name='Accuracy')
     # Start training
-    for epoch in tqdm(range(0, args.num_epochs), desc=f'Training'):
+    for epoch in tqdm(range(0, args.num_epochs), desc=f'Evaluating'):
         loss_meter.reset()
         model.train()
         p_bar=tqdm(train_loader, desc=f'Epoch {epoch}/{args.num_epochs}')
-        for idx, ((images1, images2), labels) in enumerate(p_bar):
+        
+        for idx, (images, labels) in enumerate(p_bar):
             # breakpoint()
-            model.zero_grad()
-            loss = model.forward(images1.to(args.device), images2.to(args.device))
+            classifier.zero_grad()
+            with torch.no_grad():
+                feature = model(images.to(args.device))
+            # breakpoint()
+            preds = classifier(feature)
+
+            loss = F.cross_entropy(preds, labels.to(args.device))
+            # loss = model.forward(images1.to(args.device), images2.to(args.device))
             loss.backward()
             optimizer.step()
             loss_meter.update(loss.item())
@@ -99,18 +119,22 @@ def main(args):
         lr_scheduler.step()
 
 
-        # Save checkpoint
-        os.makedirs(args.output_dir, exist_ok=True)
-        model_path = os.path.join(args.output_dir, f'{args.model}-{args.dataset}-epoch{epoch+1}.pth')
-        torch.save({
-            'epoch': epoch+1,
-            'state_dict':model.module.state_dict(),
-            # 'optimizer':optimizer.state_dict(), # will double the checkpoint file size
-            'lr_scheduler':lr_scheduler.state_dict(),
-            'args':args,
-            'loss_meter':loss_meter
-        }, model_path)
-    print(f"Model saved to {model_path}")
+        p_bar=tqdm(test_loader, desc=f'Test {epoch}/{args.num_epochs}')
+        classifier.eval()
+        correct, total = 0, 0
+        acc_meter.reset()
+        for idx, (images, labels) in enumerate(p_bar):
+            with torch.no_grad():
+                feature = model(images.to(args.device))
+                preds = classifier(feature).argmax(dim=1)
+                correct = (preds == labels.to(args.device)).sum().item()
+                acc_meter.update(correct/preds.shape[0])
+                p_bar.set_postfix({'accuracy': acc_meter.avg})
+        
+
+
+
+
 
 if __name__ == "__main__":
     main(args=get_args())
