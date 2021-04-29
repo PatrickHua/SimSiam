cf706951a3acd565e8c0071324ef3f5565b60f11
Josh Roy <josh_roy@brown.edu>
2021-04-28 21:48:25 -0400

    running ucf 101 on the full dataset

--- run_local.sh
+++ run_local.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for i in 0;
-	do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=ucf101_time_jittering_deterministic${i} && python main.py --config_file="configs/simsiam_ucf101.yaml" --data_dir="../ucfimages64x64" --log_dir="../logs/ucf101-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --small_dataset;
+for i in 0 50;
+	do export CUDA_VISIBLE_DEVICES=1,2 && export WANDB_RUN_GROUP=ucf101_time_jittering_deterministic${i} && python main.py --config_file="configs/simsiam_ucf101.yaml" --data_dir="../ucfimages64x64" --log_dir="../logs/ucf101-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --wandb;
 done
