6448b7a312c01aa3811d7dcfd4d04f0331a3e898
JeonMinkyu <jmk94810@korea.ac.kr>
2021-06-08 10:18:23 +0000

    add imagenet, imagenet100 dataset laoder

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,17 +3,23 @@ import torchvision
 from .random_dataset import RandomDataset
 
 
-def get_dataset(dataset, data_dir, transform, train=True, download=False, debug_subset_size=None):
+def get_dataset(dataset, data_dir, transform, train=True, download=True, debug_subset_size=None):
     if dataset == 'mnist':
         dataset = torchvision.datasets.MNIST(data_dir, train=train, transform=transform, download=download)
     elif dataset == 'stl10':
         dataset = torchvision.datasets.STL10(data_dir, split='train+unlabeled' if train else 'test', transform=transform, download=download)
     elif dataset == 'cifar10':
-        dataset = torchvision.datasets.CIFAR10(data_dir, train=train, transform=transform, download=download)
+        dataset = torchvision.datasets.CIFAR10(data_dir, train=train, transform=transform, download=True)
     elif dataset == 'cifar100':
         dataset = torchvision.datasets.CIFAR100(data_dir, train=train, transform=transform, download=download)
-    elif dataset == 'imagenet':
-        dataset = torchvision.datasets.ImageNet(data_dir, split='train' if train == True else 'val', transform=transform, download=download)
+    elif dataset == 'imagenet' and train == True:
+        dataset = torchvision.datasets.ImageFolder(data_dir+'train', transform=transform)  
+    elif dataset == 'imagenet' and train == False:
+        dataset = torchvision.datasets.ImageFolder(data_dir+'val', transform=transform)      
+    elif dataset == 'imagenet100' and train == True:
+        dataset = torchvision.datasets.ImageFolder(data_dir+'train', transform=transform)  
+    elif dataset == 'imagenet100' and train == False:
+        dataset = torchvision.datasets.ImageFolder(data_dir+'val', transform=transform)      
     elif dataset == 'random':
         dataset = RandomDataset()
     else:
